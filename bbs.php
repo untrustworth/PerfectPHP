065a355736748dac92ba39c038158f71e8fb7644
perfect-php <midnrendel999@gmail.com>
2019-11-04 07:00:00 +0000

    コメント投稿フォームの作成

---
+++ bbs.php
@@ -0,0 +1,15 @@
+<! DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <title>ひとこと掲示板</title>
+</head>
+<body>
+    <h1>ひとこと掲示板</h1>
+
+    <form action="bbs.php" method="post">
+        名前: <input type="text" name="name" /><br />
+        ひとこと: <input type="text" name="comment" size="60" /><br />
+        <input type="submit" name="submit" value="送信" />
+    </form>
+</body>
+</html>
