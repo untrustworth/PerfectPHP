7c35d6d799fe94620faee43cba01765a59d45824
perfect-php <midnrendel999@gmail.com>
2019-11-05 15:08:41 +0000

    PDO用の定数設定ファイルを作成した

---
+++ config/properties.php
@@ -0,0 +1,6 @@
+// 接続データベース情報(本番)
+define('DATABASE_NAME','oneline_bbs');
+define('DATABASE_USER','root');
+define('DATABASE_PASSWORD','*7B564BF5748563274AADD10561C8F4ADD0ACBDED');
+define('DATABASE_HOST','localhost');
+define('PDO_DSN','mysql:dbname=' . DATABASE_NAME .';host=' . DATABASE_HOST . '; charset=utf8');
